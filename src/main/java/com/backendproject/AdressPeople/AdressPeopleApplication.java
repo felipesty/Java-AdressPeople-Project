c4a67c64ad4fa366795ead72776d905cabdf4143
felipe <felipestylex08@gmail.com>
2023-01-13 19:22:46 -0300

    projeto criado

---
+++ src/main/java/com/backendproject/AdressPeople/AdressPeopleApplication.java
@@ -0,0 +1,13 @@
+package com.backendproject.AdressPeople;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdressPeopleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdressPeopleApplication.class, args);
+	}
+
+}
